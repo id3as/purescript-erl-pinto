59fd04bb0215f532b984909b3cd52bbaf1c10e6a
Steve Strong <steve@id3as.co.uk>
2020-06-24 17:16:12 +0100

    MessageRouter no longer doing spawn_link to prevent confusing exit messages when parent is trapping exits

--- src/Pinto/MessageRouting.erl
+++ src/Pinto/MessageRouting.erl
@@ -21,17 +21,28 @@ startRouterImpl(Ref, RegisterListener, DeregisterListener, Callback) ->
                   (DeregisterListener(Handle))(),
                   exit(normal);
                 Msg ->
-                  (Callback(Msg))(),
+                  try
+                    (Callback(Msg))()
+                  catch
+                    Class:Reason:Stack ->
+                      Recipient ! {error, {message_router_callback_failed, {Class, Reason, Stack}}},
+                      exit(error)
+                  end,
                   Fun(Handle, MonitorRef)
               end
            end,
   fun() ->
-    Pid = spawn_link(fun() ->
-                   Handle = RegisterListener(),
-                   Recipient ! { start_result, Handle },
-                   MonitorRef = monitor(process, Recipient),
-                   Fun(Handle, MonitorRef)
-               end),
+    %% We don't want spawn_link since there will be many potentially short-lived MR processes, which if the
+    %% parent is trapping exits will cause many EXIT messages without it being at all clear to the parent which
+    %% child is exiting.  We don't expect the MR process to be erroring, but if it does then it sends an error
+    %% message to the parent.  It is not expected that the parent will handle this, and so they will crash,
+    %% mirroring what would have happened if it were a spawn_link with no trap_exit
+    Pid = spawn(fun() ->
+                    Handle = RegisterListener(),
+                    Recipient ! { start_result, Handle },
+                    MonitorRef = monitor(process, Recipient),
+                    Fun(Handle, MonitorRef)
+                end),
     receive
       { start_result, Result } ->
         (Ref(Result))(Pid)
@@ -50,22 +61,28 @@ maybeStartRouterImpl(Ref, RegisterListener, DeregisterListener, Callback) ->
                   (DeregisterListener(Handle))(),
                   exit(normal);
                 Msg ->
-                  (Callback(Msg))(),
+                  try
+                    (Callback(Msg))()
+                  catch
+                    Class:Reason:Stack ->
+                      Recipient ! {error, {message_router_callback_failed, {Class, Reason, Stack}}},
+                      exit(error)
+                  end,
                   Fun(Handle, MonitorRef)
               end
            end,
   fun() ->
-    Pid = spawn_link(fun() ->
-                   MaybeHandle = RegisterListener(),
-                   case MaybeHandle of
-                     {just, Handle} ->
-                       Recipient ! { start_result, Handle },
-                       MonitorRef = monitor(process, Recipient),
-                       Fun(Handle, MonitorRef);
-                     {nothing} ->
-                       Recipient ! { start_result, undefined }
-                   end
-               end),
+    Pid = spawn(fun() ->
+                    MaybeHandle = RegisterListener(),
+                    case MaybeHandle of
+                      {just, Handle} ->
+                        Recipient ! { start_result, Handle },
+                        MonitorRef = monitor(process, Recipient),
+                        Fun(Handle, MonitorRef);
+                      {nothing} ->
+                        Recipient ! { start_result, undefined }
+                    end
+                end),
     receive
       { start_result, undefined } ->
         {nothing};
